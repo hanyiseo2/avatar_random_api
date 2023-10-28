39ce9426bab87ffba0f87cfad26da75fbf08351d
hanyiseo2 <shyshy81818181@gmail.com>
2023-10-28 11:29:34 +0900

    feat : create app

--- index.ts
+++ index.ts
@@ -1,8 +1,18 @@
-import { SVG } from "./src/SVG";
-import { clothes, hairs } from "./src/imageData";
 import Avatar from "./src/Avatar";
 import fs from "fs";
+import app from "./src/app";
 
-const svgStr = Avatar.create({ hair: 0, cloth: 1, background: 0, face: 0 });
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Server is listening on ${port}🔥`);
+});
+
+const svgStr = Avatar.create({
+  author: "Cecile",
+  hair: 0,
+  cloth: 0,
+  background: 1,
+  face: 0,
+});
 
 fs.writeFileSync("image.svg", svgStr);
