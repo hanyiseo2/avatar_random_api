e10166e8581b0087fa6805ca82892a3e050da116
hanyiseo2 <shyshy81818181@gmail.com>
2023-11-04 13:43:12 +0900

    refactor  : handlers

--- src/handlers.ts
+++ src/handlers.ts
@@ -1,7 +1,7 @@
 import Avatar from "./Avatar";
 import fs from "fs";
-import { getAssetsAndCount } from "./imageData";
-import {createRandomAssetIndex} from './util'
+import { getAssets } from "./imageData";
+import {getRandomInt} from './util'
 
 type Author = | "Cecile";
 const authors: Author[] = ["Cecile"]
@@ -9,24 +9,18 @@ const authors: Author[] = ["Cecile"]
 // const authors: Author[] = ["Cecile", "Hanyi","Hyunwoo"]
 
 export async function createRandomAvatar(req:Request, res:Response) {
-    let assetsArr = []
     let option = ["background", "hairs", "clothes", "face"]
     const randomIndex = Math.floor(Math.random()*authors.length)
-
-    for(let i =0;i<option.length;i++){
-        //[ 2, 1, 2, 1 ]
-        let randomValue = createRandomAssetIndex(getAssetsAndCount(authors[randomIndex],option[i]).count)
-        assetsArr.push(randomValue)
-    }
-    console.log(assetsArr)
-    const svgStr1 = Avatar.create({
+    const assetsArr = option.map((option)=> getRandomInt(getAssets(authors[randomIndex], option).length))
+    
+    const avatarConfig = Avatar.create({
         author: authors[randomIndex],
         background:  assetsArr[0],
         hair: assetsArr[1],
         cloth:  assetsArr[2],
         face:  assetsArr[3],
     });
-      fs.writeFileSync("image.svg", svgStr1);
+      fs.writeFileSync("image.svg", avatarConfig);
       
 }
 export async function getCreatedImage() {}
