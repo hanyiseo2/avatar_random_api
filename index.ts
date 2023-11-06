d43266eb5f47d114124bb9d451e4a66feffc1de9
hanyiseo2 <shyshy81818181@gmail.com>
2023-11-07 01:07:14 +0900

    fix the code and add more datas for avatar assets

--- index.ts
+++ index.ts
@@ -3,6 +3,8 @@ import app from "./src/app";
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server is listening on ${port}🔥`);
+  console.log(`http://localhost:${port}`)
+  console.log(`http://localhost:${port}/avatar`)
+  console.log(`http://localhost:${port}/avatar?seed=hello`)
+  console.log(`http://localhost:${port}/avatar?seed=hi`)
 });
-
-
