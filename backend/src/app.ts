2ddc63891fac2976fb6cefa92ff47d3934ee734f
hanyiseo2 <shyshy81818181@gmail.com>
2023-11-11 22:31:11 +0900

    set up cors in backend to connect with frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,21 @@
 import express from "express";
 import morgan from "morgan";
-import {
-  createAvatar,
-} from "./handlers";
+import { createAvatar } from "./handlers";
+import path from "path";
+import cors, { CorsOptions } from "cors";
 const app = express();
 
+const corsOptions: CorsOptions = {
+  origin: "http://localhost:3000",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
+
+app.use(cors(corsOptions));
+
 app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "../../frontend/src")));
 // app.get("/", frontend);
 
 /**
@@ -14,5 +24,8 @@ app.use(morgan("dev"));
  */
 app.get("/avatar", createAvatar);
 
-export default app;
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../frontend/src/index.html"));
+});
 
+export default app;
