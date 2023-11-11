1a0e90bc91fb036527ea42eec4dd79f0f86cbc4b
hanyiseo2 <shyshy81818181@gmail.com>
2023-11-11 23:39:43 +0900

    create avatarcomponent to render random avatar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,11 @@
-import React from 'react';
-
-import AvatarList from './AvatarList';
-
-const avatar1 = {
-    "Name" : "Hanyi",
-    "Author" : "Calvin",
-}
+import React from "react";
+import AvatarComponent from "./AvatarComponent";
 
 const App = () => {
-    return (
-        <div className="app">
-            <h1>Avatar Creating</h1>
-            <div className="container">
-                <AvatarList avatar1={avatar1}/>                    
-            </div>
-        </div>
-    );
-}
-
+  return (
+    <div>
+      <AvatarComponent />
+    </div>
+  );
+};
 export default App;
