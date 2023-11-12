1b9827eaeb8224eea534d910fce9efa202ed63ed
hanyiseo2 <shyshy81818181@gmail.com>
2023-11-13 01:33:36 +0900

    seperate header and update folder location

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="container">
+        <h2> Avatar </h2>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
